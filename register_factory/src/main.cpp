eccdee9b45375aabe1b478f22760c53d6b3edb2e
miaochenlu <jones@DESKTOP-60SP3JA.localdomain>
2022-05-09 14:58:31 +0800

    modify: register factory use shared_ptr

--- register_factory/src/main.cpp
+++ register_factory/src/main.cpp
@@ -4,11 +4,7 @@ using namespace register_factory::factory;
 using namespace register_factory::common;
 int main() {
     Factory& factory = Factory::GetInstance();
-    // BasePtr base;
-    // base.reset(factory.GetObject("derived1"));
-    // base->DoProcess();
-    Base* base;
-    base = factory.GetObject("derived1");
+    BasePtr base;
+    base.reset(factory.GetObject("derived1"));
     base->DoProcess();
-
 }
